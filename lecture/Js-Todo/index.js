292d5469ffcaec74b0698e402f3604149fca7646
aoha <aoha.kurobane168@gmail.com>
2022-09-29 22:13:45 +0900

    ToDoアプリのindex.jsを修正

--- lecture/Js-Todo/index.js
+++ lecture/Js-Todo/index.js
@@ -1,4 +1,4 @@
-gconst onClickAdd = () => {
+const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
